47b9e958b12813033ff7c58e33fd632db56012c3
Mason3144 <lts890303@gmail.com>
2023-04-29 01:44:43 +0900

    first commit

---
+++ src/main/java/com/jpa/n_plus_one_solution/NPlusOneSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.n_plus_one_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NPlusOneSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NPlusOneSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/n_plus_one_solution/NPlusOneSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.n_plus_one_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NPlusOneSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
