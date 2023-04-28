920aafac384abf15d1d20c758e49d414d3e33203
Mason3144 <lts890303@gmail.com>
2023-04-29 05:40:55 +0900

    Example for Spring Data JPA

--- src/main/java/com/jpa/n_plus_one_solution/repository/MemberRepository.java
+++ src/main/java/com/jpa/n_plus_one_solution/repository/MemberRepository.java
@@ -1,2 +1,12 @@
-package com.jpa.n_plus_one_solution.repository;public class MemberRepository {
+package com.jpa.n_plus_one_solution.repository;
+
+import com.jpa.n_plus_one_solution.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    @Query("SELECT DISTINCT m FROM Member m JOIN FETCH m.orders o WHERE m.memberId = :memberId")
+    public List<Member> findMemberAndOrder(Long memberId);
 }
